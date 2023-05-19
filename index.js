23ed049b4137566f9d7cd6c90553b77e872eed90
Donatella Crisante <dona.crisante@zoho.com>
2023-05-19 12:50:10 +0200

    create event listener

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+const maxPage = 42;
+const minPage = 1;
 const page = 1;
+/* page.max = maxPage;
+page.min = minPage; */
+console.log(page);
 const searchQuery = "";
 
 export async function fetchCharacters() {
   //const createCharacterCard = createCharacterCard();
-  const ApiUrl = `https://rickandmortyapi.com/api/character`;
+  const ApiUrl = `https://rickandmortyapi.com/api/character?page=${page}`;
 
   try {
     const response = await fetch(ApiUrl);
@@ -33,5 +37,18 @@ export async function fetchCharacters() {
   console.log("An error occured", error);
 }
 }
-
 fetchCharacters();
+
+prevButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  cardContainer.innerHTML = "";
+  page--; 
+  fetchCharacters();
+})
+
+nextButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  cardContainer.innerHTML = "";
+  page++; 
+  fetchCharacters();
+})
