8515b80cfd3dd11408c32ca143af1e3e91e434b0
Antje Witte <antjewitte90@gmail.com>
2023-05-22 11:40:20 +0200

    searchBar

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 import { createCharacterCard } from "./components/card/card.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+//const searchBarContainer = document.querySelector(
+//  '[data-js="search-bar-container"]'
+//);
 const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
+const searchBarInput = document.querySelector('[data-js="search-bar-input"]');
+//const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
 export async function fetchCharacters() {
-  //const createCharacterCard = createCharacterCard();
-  const ApiUrl = `https://rickandmortyapi.com/api/character?page=${page}`;
+  const ApiUrl = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
 
   try {
     const response = await fetch(ApiUrl);
     const data = await response.json();
-  console.log(data);
-  const maxPage = data.info.pages;
-  pagination.innerHTML = `${page}/${maxPage}`;
+    console.log(data);
+    const maxPage = data.info.pages;
+    pagination.innerHTML = `${page}/${maxPage}`;
 
-  data.results.forEach((character) => {
-    const characterElement = createCharacterCard(character);
-    cardContainer.append(characterElement);
-    //const ul = document.createElement("ul");
-  }); 
-} catch (error) {
-  console.log("An error occured", error);
-}
+    data.results.forEach((character) => {
+      const characterElement = createCharacterCard(character);
+      cardContainer.append(characterElement);
+      //const ul = document.createElement("ul");
+    });
+  } catch (error) {
+    console.log("An error occured", error);
+  }
 }
 fetchCharacters();
 
 prevButton.addEventListener("click", async () => {
   cardContainer.innerHTML = "";
-  page--; 
+  page--;
   await fetchCharacters();
-})
+});
 
 nextButton.addEventListener("click", async () => {
   cardContainer.innerHTML = "";
-  page++; 
+  page++;
   console.log(page);
   await fetchCharacters();
-})
+});
+
+searchBar.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  cardContainer.innerHTML = "";
+  searchQuery = searchBarInput.value;
+
+  await fetchCharacters();
+});
